13a6156578fbb0d50a36adf47bc53dde64185313
arnoldsu <arnoldsu@mit.edu>
2024-12-01 10:48:14 -0500

    various augmentations: sampling multiple actions per simuation, optimizing valid actions, and filling all unvisited nodes

--- games.py
+++ games.py
@@ -48,6 +48,7 @@ class PTTTGame:
         return True, [0, 0]
 
     def next_state(self, infosets, actions):
+        infosets = infosets.copy()
         board = self._build_board(infosets)
         for i in range(len(actions)):
             infosets[i] += str(actions[i])
@@ -61,7 +62,12 @@ class PTTTGame:
 
         return infosets, rewards, terminal
 
-    def choose_uniform_action(self, infoset, player):
+    def choose_uniform_action(self, infoset, player, sample=False):
+        SAMPLE_SIZE = 3
+        if sample:
+            valid_actions = self.valid_actions(infoset, player)
+            return np.random.choice(valid_actions, size=min(SAMPLE_SIZE, len(valid_actions)), replace=False), 1/len(valid_actions) * np.ones(min(SAMPLE_SIZE, len(valid_actions)))
+
         valid_actions = self.valid_actions(infoset, player)
         return np.random.choice(valid_actions), 1/len(valid_actions)
 
@@ -73,5 +79,57 @@ class PTTTGame:
         for i in range(9):
             if str(i) not in infoset:
                 valid_actions.append(i)
+        
+        if player == 0: # to augment strategies, such that if opp has winning spot then block it immediately
+            board = self._build_player_board(infoset, player)
+            opp_spots = self._find_winning_spots(board, 1)
+            # player_spots = self._find_winning_spots(board, 0)
+            if len(opp_spots) > 0:
+                valid_actions = opp_spots
+            # elif len(player_spots) > 0:
+            #     valid_actions = player_spots
 
         return np.array(valid_actions)
+
+    def _build_player_board(self, infoset, player):
+        opp = 1 - player
+        board = [None for _ in range(9)]
+        for i in range(1, len(infoset), 2):
+            action, outcome = infoset[i:i+2]
+            if outcome == '*':
+                board[int(action)] = player
+            else:
+                board[int(action)] = opp
+        return board
+    
+    def _find_winning_spots(self, board, player):
+        spots = []
+        for i in range(3):
+            if board[3*i + 0] == player and board[3*i + 1] == player and board[3*i + 2] == None:
+                spots.append(3*i + 2)
+            elif board[3*i + 1] == player and board[3*i + 2] == player and board[3*i + 0] == None:
+                spots.append(3*i + 0)
+            elif board[3*i + 0] == player and board[3*i + 2] == player and board[3*i + 1] == None:
+                spots.append(3*i + 1)
+            elif board[i + 3*0] == player and board[i + 3*1] == player and board[i + 3*2] == None:
+                spots.append(i + 3*2)
+            elif board[i + 3*1] == player and board[i + 3*2] == player and board[i + 3*0] == None:
+                spots.append(i + 3*0)
+            elif board[i + 3*0] == player and board[i + 3*2] == player and board[i + 3*1] == None:
+                spots.append(i + 3*1)
+        
+        if board[0] == player and board[4] == player and board[8] == None:
+            spots.append(8)
+        elif board[0] == player and board[8] == player and board[4] == None:
+            spots.append(4)
+        elif board[4] == player and board[8] == player and board[0] == None:
+            spots.append(0)
+
+        if board[2] == player and board[4] == player and board[6] == None:
+            spots.append(6)
+        elif board[2] == player and board[6] == player and board[4] == None:
+            spots.append(4)
+        elif board[4] == player and board[6] == player and board[2] == None:
+            spots.append(2)
+
+        return np.array(spots)
--- mcts.py
+++ mcts.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 
+SAMPLE_SIZE = 3
 class Node(object):
     def __init__(self, n, depth, n_actions, valid_actions) -> None:
         self.n = n
@@ -9,10 +10,15 @@ class Node(object):
         self.rewards[valid_actions] = 1
         self.valid_actions = valid_actions
         self.update_probabilities()
+        # print(self.depth, self.valid_actions)
+        # assert self.depth == len()#10 - len(valid_actions)
 
-        assert self.depth == 10 - len(valid_actions)
+    def choose_action(self, sample=False):
+        if sample:
+            p = self.probabilities / np.sum(self.probabilities)
+            actions = np.random.choice(len(p), size=min(SAMPLE_SIZE, len(self.valid_actions)), p=p, replace=False)
+            return actions, p[actions]
 
-    def choose_action(self):
         p = self.probabilities / np.sum(self.probabilities)
         action = np.random.choice(len(p), p=p)
         return action, p[action]
@@ -20,15 +26,21 @@ class Node(object):
     def update_reward(self, reward, action, prob):
         assert action in self.valid_actions
 
-        self.rewards[action] *= np.exp(1.7**(self.depth - 9) * reward / prob)
-        self.rewards /= np.sum(self.rewards)
+        new_action_reward = self.rewards[action] * np.exp(1.7**(self.depth - 9) * reward / prob)
+        if new_action_reward != 0:
+            self.rewards[action] = new_action_reward
+
+        ## NOTE: when reward blows up to inf, make it deterministic by setting all other rewards to 0
+        if np.any(np.isposinf(self.rewards)):
+            new_rewards = np.zeros_like(self.rewards, dtype=int) + 10 ** -50
+            new_rewards[np.isposinf(self.rewards)] = 1 
+            self.rewards = np.copy(new_rewards)
 
         self.update_probabilities()
     
     def update_probabilities(self):
-        # self.probabilities = self.rewards / np.sum(self.rewards)
-        # self.probabilities *= (1 - self.n**(-0.3))
-        self.probabilities = self.rewards * (1 - self.n**(-0.3))
+        self.probabilities = self.rewards / np.sum(self.rewards)
+        self.probabilities *= (1 - self.n**(-0.3))
         self.probabilities[self.valid_actions] += self.n**(-0.3) / len(self.valid_actions)
     
     def strategy_vector(self):
@@ -40,18 +52,83 @@ class MCTS(object):
         self.num_iters = num_iters
         self.nodes = [{} for _ in range(len(game.roots))]
 
-    def solve(self):
+    def solve(self, sample=False):
         for n in range(self.num_iters):
-            self.simulate(n+1)
+            self.simulate(n+1, sample=sample)
+            if n % 10000 == 0:
+                print(n, 'done out of', self.num_iters)
+    
+    def traverse_tree(self, curr_infosets, in_tree, paths, n, depth, terminal):
+        n_players = self.game.n_players
+        actions = []
+        add_to_path = [val for val in in_tree]
+        for i in range(n_players):
+            if not in_tree[i]:
+                action_samples, prob_samples = self.game.choose_uniform_action(curr_infosets[i], i, sample=False)
+                action_samples = [action_samples]
+                prob_samples = [prob_samples]
+            else:
+                if curr_infosets[i] in self.nodes[i]:
+                    curr_node = self.nodes[i][curr_infosets[i]]
+                    action_samples, prob_samples = curr_node.choose_action(sample=True)
+                else:
+                    action_samples, prob_samples = self.game.choose_uniform_action(curr_infosets[i], i, sample=True)
+                    assert depth == (len(curr_infosets[i]) - 1) // 2 + 1
+                    curr_node = Node(n, depth, self.game.n_actions(curr_infosets[i]), self.game.valid_actions(curr_infosets[i], i))
+                    self.nodes[i][curr_infosets[i]] = curr_node
+                    in_tree[i] = False
 
-    def simulate(self, n):
+                # paths[i].append((curr_infosets[i], action, prob))
+            
+            actions.append((action_samples, prob_samples))
+
+        actions_0, probs_0 = actions[0]
+        actions_1, probs_1 = actions[1]
+        for i in range(len(actions_0)):
+            action_0, prob_0 = actions_0[i], probs_0[i]
+
+            if add_to_path[0]:
+                paths[0].append((curr_infosets[0], action_0, prob_0))
+
+            for j in range(len(actions_1)):
+                action_1, prob_1 = actions_1[j], probs_1[j]
+
+                if add_to_path[1]:
+                    paths[1].append((curr_infosets[1], action_1, prob_1))
+
+                # print('ITER:', n, 'DEPTH:', depth, 'action idx:', j, 'curr_infosets', curr_infosets)
+                new_infosets, rewards, terminal = self.game.next_state(curr_infosets, [action_0, action_1])
+
+                if terminal:
+                    # print('FINAL PATHS:', paths, ' DEPTH:', depth, ' ITER:', n)
+                    # print('FINAL infoset', new_infosets, 'REWARDS:', rewards)
+                    for k in range(n_players):
+                        for infoset, action, prob in paths[k][::-1]:
+                            self.nodes[k][infoset].update_reward(rewards[k], action, prob)
+                else:
+                    # print('starting next traversal')
+                    self.traverse_tree(new_infosets, in_tree, paths, n, depth+1, terminal)
+                    # print('completed traversal')
+
+                if add_to_path[1]:
+                    paths[1] = paths[1][:-1]
+            
+            if add_to_path[0]:
+                paths[0] = paths[0][:-1]
+
+    def simulate(self, n, sample=False):
+        # if sample: when simulating / traversing tree, sample multiple actions instead of just one per simulation
         n_players = self.game.n_players
         in_tree = [True for _ in range(n_players)]
         curr_infosets = [root for root in self.game.roots]
         paths = [[] for _ in range(n_players)]
         terminal = False
-
         depth = 1
+
+        if sample:
+            self.traverse_tree(curr_infosets, in_tree, paths, n, depth, terminal)
+            return
+
         while not terminal:
             actions = []
             for i in range(n_players):
@@ -63,6 +140,9 @@ class MCTS(object):
                         action, prob = curr_node.choose_action()
                     else:
                         action, prob = self.game.choose_uniform_action(curr_infosets[i], i)
+
+                        assert depth == (len(curr_infosets[i]) - 1) // 2 + 1
+
                         curr_node = Node(n, depth, self.game.n_actions(curr_infosets[i]), self.game.valid_actions(curr_infosets[i], i))
                         self.nodes[i][curr_infosets[i]] = curr_node
                         in_tree[i] = False
@@ -78,11 +158,36 @@ class MCTS(object):
             for infoset, action, prob in paths[i][::-1]:
                 self.nodes[i][infoset].update_reward(rewards[i], action, prob)
     
-    def get_strategy(self, player, infoset_txt_file):
+    def get_strategy(self, player, infoset_txt_file, alpha=None):
+        # alpha controls how much to weigh strategies from already seen infosets
+
         lines = open(infoset_txt_file).readlines()
         # Initially, set the tensor with all ones. We will mask out the illegal actions below,
         # and then we will normalize row-wise just before saving the tensor below.
+
+        print('preprocessing infosets') # groups all infosets with same set of * (success) and . (fail)
+        if alpha is not None:
+            mapping_infosets = {}
+            reduced_infosets = {}
+            for idx, line in enumerate(lines):
+                line = line.strip()
+                successful = []
+                failed = []
+                for i in range(1, len(line), 2):
+                    if line[i+1] == '*':
+                        successful.append(int(line[i]))
+                    else:
+                        failed.append(int(line[i]))
+                successful.sort()
+                failed.sort()
+                mapping_infosets[line] = (tuple(successful), tuple(failed))
+                reduced_infosets[(tuple(successful), tuple(failed))] = (np.zeros(9), 0)
+
+                if idx % 1000000 == 0:
+                    print(idx, 'done out of', len(lines))
+
         tensor = np.ones((len(lines), 9), dtype=np.float32)
+        print('filling strategies for nodes found in tree')
         for idx, line in enumerate(lines):
             line = line.strip() # Remove the \n
             assert line.startswith('|')
@@ -92,6 +197,27 @@ class MCTS(object):
 
             if line in self.nodes[player]:
                 tensor[idx] = self.nodes[player][line].strategy_vector()
+                if alpha is not None:
+                    strat, count = reduced_infosets[mapping_infosets[line]]
+                    reduced_infosets[mapping_infosets[line]] = (strat + tensor[idx], count + 1)
+            
+            if idx % 1000000 == 0:
+                print(idx, 'done out of', len(lines))
+        
+        print('filling strategies for nodes not found in tree')
+        for idx, line in enumerate(lines):
+            line = line.strip() # Remove the \n
+            assert line.startswith('|')
+            n = len(line) - 1
+            assert n % 2 == 0
+            n = n // 2
+
+            if alpha is not None:
+                if line not in self.nodes[player]:
+                    strat, count = reduced_infosets[mapping_infosets[line]]
+                    n_actions = len(self.game.valid_actions(line, player))
+                    if count > 0 and n_actions > 0:
+                        tensor[idx] = alpha * strat / count + (1-alpha) * 1/n_actions
 
             for j in range(n):
                 pos = line[1 + 2 * j]
@@ -105,6 +231,7 @@ class MCTS(object):
 
             if idx % 1000000 == 0:
                 print(idx, 'done out of', len(lines))
+
         # Renormalize row wise
         tensor /= np.sum(tensor, axis=1)[:,None]
         np.save(f'pttt_pl{player}.npy', tensor)
